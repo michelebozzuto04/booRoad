67eb3a05249b4622b743b472c37cbec9b9378478
francescoboschelle <francibossch@gmail.com>
2025-08-26 15:56:56 +0200

    Enhance SingleTripPage with trip details and companions display

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import DefaultLayout from './layouts/DefaultLayout'
-import HomePage from './pages/HomePage'
-import TripsPage from './pages/TripsPage'
-import SingleTripPage from './pages/SingleTripPage'
-import CompanionsPage from './pages/CompanionsPage'
-import SingleCompanionPage from './pages/SingleCompanionPage'
-import NotFoundPage from './pages/NotFoundPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import HomePage from "./pages/HomePage";
+import TripsPage from "./pages/TripsPage";
+import SingleTripPage from "./pages/SingleTripPage";
+import CompanionsPage from "./pages/CompanionsPage";
+import SingleCompanionPage from "./pages/SingleCompanionPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout />}>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/trips' element={<TripsPage />} />
-          <Route path='/trips/:id' element={<SingleTripPage />} />
-          <Route path='/companions' element={<CompanionsPage />} />
-          <Route path='/companions/:id' element={<SingleCompanionPage />} />
-          <Route path='*' element={<NotFoundPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/trips" element={<TripsPage />} />
+          <Route path="/trips/:id" element={<SingleTripPage />} />
+          <Route path="/companions" element={<CompanionsPage />} />
+          <Route path="/companions/:id" element={<SingleCompanionPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/SingleTripPage.jsx
+++ src/pages/SingleTripPage.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { trips, companions } from "../assets/data";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function SingleTripPage() {
-  return <div>SingleTripPage</div>;
+  const { id } = useParams();
+  const trip = trips.find((trip) => trip.id === parseInt(id));
+
+  const tripCompanions = trip.companions_id.map((companionId) => {
+    return companions.find((companion) => companion.id === companionId);
+  });
+
+  return (
+    <>
+      <div className="container">
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={trip.image}
+                className="img-fluid rounded-start h-100"
+                alt={trip.title}
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title mb-4">{trip.title}</h5>
+                <p className="card-text">{trip.description}</p>
+                <p className="card-text">Inizio: {trip.start_date}</p>
+                <p className="card-text">Fine: {trip.end_date}</p>
+                <p>Prezzo: {trip.price}€</p>
+                <h6>Accompagnatori</h6>
+                <div className="row">
+                  {tripCompanions.map((companion) => {
+                    return (
+                      <>
+                        <div className="col-4" key={companion.id}>
+                          <Link to={`/companions/${companion.id}`}>
+                            <p className="card-title mb-4">
+                              {companion.name} {companion.surname}
+                            </p>
+                          </Link>
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SingleTripPage;
